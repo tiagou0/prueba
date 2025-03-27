f6e1e1ba7852fdbb6fab0d717fd9a52ee3d7129a
tiagou0 <tiagoucerp@gmail.com>
2025-03-27 15:48:20 -0300

    concha

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let valorIngresado = parseInt(prompt("Ingrese un valor para determinar si es par o impar: "));
 
 document.write("El valor ingresado es " + valorIngresado + "<br>");
 
 function determinarParImpar() {
+    let valorIngresado = parseInt(prompt("Ingrese un valor para determinar si es par o impar: "));
     if (valorIngresado % 2 == 0) {
         document.write("El valor ingresado es par");
     } else if (isNaN(valorIngresado)) {
@@ -12,10 +12,29 @@ function determinarParImpar() {
     }
 }
 
-determinarParImpar()
 
-let arrayNumbers = [2,3,4,5,6,7];
+function notifyMe() {
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification("Hi there!");
+      // …
+    } else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          const notification = new Notification("Hi there!");
+          // …
+        }
+      });
+    }
+  
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+  }
 
-for (let i = arrayNumbers[0]; i = arrayNumbers.length; i++) {
-    document.write("<br>" + i)
-}
+Notification.requestPermission()
